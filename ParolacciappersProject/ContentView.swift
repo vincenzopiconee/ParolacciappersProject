22de7932d9905325a015ad106ab00d955c2b069a
vincenzopiconee <vincen.picone@studenti.unina.it>
2025-02-21 15:19:57 +0100

    Initial Commit

---
+++ ParolacciappersProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParolacciappersProject
+//
+//  Created by Vincenzo Picone on 21/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParolacciappersProject/ParolacciappersProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParolacciappersProjectApp.swift
+//  ParolacciappersProject
+//
+//  Created by Vincenzo Picone on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ParolacciappersProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
